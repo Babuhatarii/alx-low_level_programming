bc8d7a3859dfd8ba0ed1742e095d6d7e9580ca4e
Babuhatarii <ochegoharyon@gmail.com>
2023-06-29 04:49:58 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
-
-	/*
-	 * Update the value at p + 5 with 98
-	 * This indirectly modifies a[2] by changing the value at memory location
-	 * p + 5, assuming that the memory locations are contiguous.
-	 */
-	*(p + 5) = 98;
-
-	/* Print the updated value of a[2] */
-	printf("a[2] = %d\n", a[2]);
-
-	return (0);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
