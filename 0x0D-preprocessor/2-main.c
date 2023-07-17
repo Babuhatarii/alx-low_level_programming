29e6976e9eda3c093595d91d00121912b92d4447
Babuhatarii <ochegoharyon@gmail.com>
2023-07-17 05:15:15 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - prints the name of the file
  *
- * Description: Prints the name of the file it was compiled from
- * using the standard library.
- *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
-	return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
 
