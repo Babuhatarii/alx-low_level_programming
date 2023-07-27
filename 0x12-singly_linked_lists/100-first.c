3af49b9b785ea11ab68df40dac6b2981c115b349
Babuhatarii <ochegoharyon@gmail.com>
2023-07-27 08:29:17 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-/**
- * first - Function that prints a sentence before the main function is executed
- *
- * Description: function is tagged with the 'constructor' attribute, which
- *	that it is automatically executed before the 'main' function
- */
 void first(void) __attribute__ ((constructor));
 
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
 void first(void)
+
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
-
-/**
- * main - Entry point of the program.
- *
- * Return: 0 on success.
- */
-int main(void)
-{
-	/* Rest of the main function code goes here... */
-
-	return (0);
-}
-
