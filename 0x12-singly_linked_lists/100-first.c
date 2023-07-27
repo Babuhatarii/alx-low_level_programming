d6581cfb13d392b61d45ef1e646a24b2fe90661b
Babuhatarii <ochegoharyon@gmail.com>
2023-07-27 08:13:44 -0700

    betty 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_lines - Function to print the specified lines.
+ */
+void print_lines(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	/* Call the function to print the lines */
+	print_lines();
+
+	/* Rest of the main function code goes here... */
+
+	return (0);
+}
+
